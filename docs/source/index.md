29eb78a86858fc6e5fe45dab0a2298248be31c46
Lukáš Málek <malek.luky@gmail.com>
2024-01-05 23:50:35 +0100

    Initial Commit

---
+++ docs/source/index.md
@@ -0,0 +1,4 @@
+## Documentation
+
+Documentation for Wheel_Assembly_Detection
+
---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from Wheel_Assembly_Detection.models.model import MyNeuralNet
+from Wheel_Assembly_Detection.predict_model import predict
