3c42306cac085b64d5589a0f86c8e43e94266fd8
Lukáš Málek <malek.luky@gmail.com>
2024-01-11 09:35:16 +0100

    Replaced _DATA_PATH

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,23 +1,25 @@
 import os
 import torch
 import pytest
-from tests import _PATH_DATA
+
+# Get the absolute path to the data directory
+data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
 
 def test_folder_path_exists():
-    assert os.path.exists(_PATH_DATA), "The _PATH_DATA folder does not exist, please run dvc pull"
+    assert os.path.exists(data_dir), "The data folder does not exist, please run dvc pull"
 
 def test_folder_contents():
 
     # Check if there are more than 90 items in the folder
-    folder_path = _PATH_DATA+"/processed"
+    folder_path = os.path.join(data_dir, "processed")
     assert len(os.listdir(folder_path)) > 90, "There are not enough items in the folder, did dvc pull run correctly?"
 
     # Check if there are more than 90 items in the folder
-    folder_path = _PATH_DATA+"/raw"
-    assert len(os.listdir(folder_path)) > 10, "The original meassurements are missing, maybe try to run dvc pull?"
+    folder_path = os.path.join(data_dir, "raw")
+    assert len(os.listdir(folder_path)) > 10, "The original measurements are missing, maybe try to run dvc pull?"
 
     # Load one of the items
-    item_path = os.path.join(_PATH_DATA, "processed/data_id_1_label_True.pt")
+    item_path = os.path.join(data_dir, "processed/data_id_1_label_True.pt")
     item = torch.load(item_path)
 
     # Check if it is a torch file
