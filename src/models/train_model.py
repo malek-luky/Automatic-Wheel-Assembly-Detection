7b0c7fccbc770b394f3bf2f0053e57ff86b17314
Lukas Rasocha <rasocha.lukas@gmail.com>
2024-01-16 12:56:25 +0100

    add secret manager and bucket loading to our main training script

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,48 +1,54 @@
 import click
+import numpy as np
+import torch
+import pandas as pd
+import wandb
+import os
+
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 
 from datetime import datetime
-import pandas as pd
 from sklearn.model_selection import train_test_split
-import numpy as np
-import torch
 from torch.utils.data import DataLoader, TensorDataset
-import wandb
 
 from src.models.model import TireAssemblyLSTM
-#from model import TireAssemblyLSTM
+from src.helper.gcp_utils import load_bucket_data, get_secret
 
 # WANDB SETUP
 WANDB_DEFINED = False
 WANDB_PROJECT = None
-WANDB_ENTITY  = None
+WANDB_ENTITY = None
 SWEEP_DEFINED = False
+WANDB_API_KEY = get_secret('wheel-assembly-detection', 'WANDB_API_KEY')
+
+# Set the environment variable
+os.environ["WANDB_API_KEY"] = WANDB_API_KEY
 
 # Load dataset (call script from root directory -> python src/models/train_model.py)
-df = pd.read_csv('data/processed/dataset_concatenated.csv')
+df = load_bucket_data('wheel-assembly-detection-dataset', 'data/processed/dataset_concatenated.csv')
 
 
 def create_sequences(df, seq_length):
-        sequences = []
-        labels = []
+    sequences = []
+    labels = []
 
-        for _, group in df.groupby('#Identifier'):
-            data = group.values
-            for i in range(len(data) - seq_length):
-                seq = data[i:(i + seq_length), :-2]  # All columns except Identifier and Label
-                label = data[i + seq_length - 1, -1]  # Label of the last time step in the sequence
-                sequences.append(seq)
-                labels.append(label)
+    for _, group in df.groupby('#Identifier'):
+        data = group.values
+        for i in range(len(data) - seq_length):
+            seq = data[i:(i + seq_length), :-2]  # All columns except Identifier and Label
+            label = data[i + seq_length - 1, -1]  # Label of the last time step in the sequence
+            sequences.append(seq)
+            labels.append(label)
 
-        return np.array(sequences), np.array(labels)
-  
+    return np.array(sequences), np.array(labels)
+
 
 def sweep_hparams(iterations: int) -> None:
     global SWEEP_DEFINED
     SWEEP_DEFINED = True
-    
+
     # current version only performs parameter sweeping on the number of hidden layers
     # by minimizing the training loss
     # choice of parameters can be added to the click interface
@@ -67,7 +73,7 @@ def sweep_hparams(iterations: int) -> None:
         sweep=sweep_config, project=WANDB_PROJECT, entity=WANDB_ENTITY
     )
     wandb.agent(sweep_id, train_routine, count=iterations)
-    
+
 
 def train_routine(config=None) -> None:
     # if hyperparameters sweeping is defined, the config is loaded from wandb
@@ -84,7 +90,7 @@ def train_routine(config=None) -> None:
             wandb_logger = WandbLogger(project=WANDB_PROJECT, entity=WANDB_ENTITY)
         else:
             wandb_logger = None
-        
+
     # Create sequences for each experiment
     X_seq, y_seq = create_sequences(df, hparams.sequence_length)
 
@@ -112,7 +118,6 @@ def train_routine(config=None) -> None:
     train_loader = DataLoader(train_data, batch_size=hparams.batch_size, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=hparams.batch_size)
 
-
     # Train the model
     trainer = Trainer(
         logger=wandb_logger,
@@ -132,29 +137,30 @@ def train_routine(config=None) -> None:
     # create a run and log a model artifact to it
     with wandb.init(project="automatic-wheel-assembly-detection") as run:
         model_artifact = wandb.Artifact(
-        name="mlops_model", 
-        type="model"
+            name="mlops_model",
+            type="model"
         )
         model_artifact.add_file(f'models/model_{time}.pth')
-        run.log_artifact(model_artifact) # logs the model version "my_model:v0"
+        run.log_artifact(model_artifact)  # logs the model version "my_model:v0"
+
 
 @click.command()
 @click.option('--train', is_flag=True, default=True, help='Use to only train the model.')
 @click.option('--sweep', is_flag=True, default=False, help='Use to sweep hyperparameters.')
 @click.option('--sweep_iter', default=5, help='Number of iterations for hyperparameters sweeping.')
-@click.option('--wandb_on', is_flag=True, default=False, help='Use to connect to Wandb service. Automatically set to True if --sweep is defined. Otherwise False')
-@click.option('--wandb_project', default="automatic-wheel-assembly-detection", 
+@click.option('--wandb_on', is_flag=True, default=False,
+              help='Use to connect to Wandb service. Automatically set to True if --sweep is defined. Otherwise False')
+@click.option('--wandb_project', default="automatic-wheel-assembly-detection",
               help='Your wandb project name. Default is "automatic-wheel-assembly-detection"')
-@click.option('--wandb_entity', default="02476mlops", 
+@click.option('--wandb_entity', default="02476mlops",
               help='Your wandb entity name. Default is "02476mlops"')
+def parse_input(train: bool, sweep: bool, sweep_iter: int, wandb_on: bool, wandb_project: str, wandb_entity: str) -> None:
 
-def parse_input(train: bool, sweep:bool, sweep_iter:int, wandb_on: bool, wandb_project: str, wandb_entity: str) -> None:
-    
     global WANDB_PROJECT, WANDB_ENTITY, WANDB_DEFINED
     WANDB_DEFINED = wandb_on
     WANDB_PROJECT = wandb_project
     WANDB_ENTITY = wandb_entity
-    
+
     if sweep:
         print("Sweeping hyperparameters with", sweep_iter, "iteration(s).")
         sweep_hparams(sweep_iter)
@@ -164,4 +170,3 @@ def parse_input(train: bool, sweep:bool, sweep_iter:int, wandb_on: bool, wandb_p
 
 if __name__ == '__main__':
     parse_input()
-    
